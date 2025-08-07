0e562c42e5943d1e65f5847fab852bf6921a7cd3
isfarMohi <isfarmohi.im@gmail.com>
2025-08-07 14:59:18 +0530

    dump unnecessary files

--- transcribe_module/assembly_test.py
+++ transcribe_module/assembly_test.py
@@ -5,17 +5,13 @@ aai.settings.api_key = "f31459fc0dbe4172aaf5da7d2bedf585"
 transcriber = aai.Transcriber()
 
 def transcribe():
-
     try:
         transcript = transcriber.transcribe(
             "upload\F_1201_12y11m_1.mp3",
             config=aai.TranscriptionConfig(speaker_labels=True)
         )
-
         return {
-            "full_transcript": transcript.text,
-            
+            "full_transcript": transcript.text,       
         }
-
     except Exception as e:
         return {"error": str(e)}
--- transcribe_module/transcribe_test.py
+++
@@ -1,5 +0,0 @@
-import whisper
-
-model = whisper.load_model("base")
-result = model.transcribe(r"upload\Chris_Home_2.mp3")
-print(result["text"])
